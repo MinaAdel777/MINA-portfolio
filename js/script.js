ffc44acef337950d12cbd6c87cf7babb158b8c4b
MINA ADEL <minaadel837@gmail.com>
2025-02-19 17:28:57 +0200

    upload

---
+++ js/script.js
@@ -0,0 +1,68 @@
+/*==================== toggle icon navbar ====================*/
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("bx-x-circle");
+  navbar.classList.toggle("active");
+});
+/*==================== scroll sections active link ====================*/
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((section) => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 150;
+    let height = section.offsetHeight;
+    let id = section.getAttribute("id");
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+
+  /*==================== sticky navbar ====================*/
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > window.innerHeight - 68);
+
+  /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+  menuIcon.classList.remove("bx-x-circle");
+  navbar.classList.remove("active");
+};
+
+/*==================== scroll reveal ====================*/
+ScrollReveal({
+  // reset: true,
+  distance: "70px",
+  duration: 1000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .skills-container, .portfolio-box, .contact form",
+  {
+    origin: "bottom",
+  }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+/*==================== typed js ====================*/
+const typed = new Typed(".multiple-text", {
+  strings: [
+    "MEAN Stack Web Developer!",
+    "Frontend Developer!",
+    "Backend Developer!",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1500,
+  loop: true,
+});
+
+// ! -----> M I N A <-----
